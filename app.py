1fcee4c4c5a2f216e2dbd74553e0bd7c2a7f106e
VerM1chel <86617861+VerM1chel@users.noreply.github.com>
2022-02-20 23:51:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-import config
 import flask
 from flask import Flask
 import torch
 from model import BertSentimentClassifier
 from flask import request
+
+import config
 
 app = Flask(__name__)
 
@@ -11,54 +12,48 @@ def sentence_prediction(sentence):
     sentence = str(sentence)
     sentence = ' '.join(sentence.split())
 
-    input_ids = config.tokenizer.encode_plus(
+    input_ids = config.TOKENIZER.encode_plus(
             sentence,
             None,
             add_special_tokens=True,
-            max_length=config.max_length,
-            pad_to_max_length=True
-        )
+            max_length=config.MAX_LENGTH,
+            truncation=True
+    )
+
     ids = input_ids['input_ids']
     mask = input_ids['attention_mask']
 
-    padding_length = config.max_len - len(ids)
-    ids = ids + [0] * padding_length
-    mask = mask + [0] * padding_length
+    padding_length = config.MAX_LENGTH - len(ids)
+    ids = ids + ([0] * padding_length)
+    mask = mask + ([0] * padding_length)
 
-    ids = ids.to(config.device, dtype=torch.long)
-    mask = mask.to(config.device, dtype=torch.long)
+    ids = torch.tensor(ids, dtype=torch.long).unsqueeze(0)
+    mask = torch.tensor(mask, dtype=torch.long).unsqueeze(0)
 
-    ids = torch.tensor(ids, dtype=torch.long)
-    mask = torch.tensor(mask, dtype=torch.long)
-    outputs = model(ids=ids, mask=mask)
+    ids = ids.to(config.DEVICE, dtype=torch.long)
+    mask = mask.to(config.DEVICE, dtype=torch.long)
+
+    outputs = model(ids=ids, attention_mask=mask)
     outputs = torch.sigmoid(outputs).cpu().detach().numpy()
     return outputs[0][0]
 
-
-@app.route('/predict')
+@app.route("/")
 def predict():
-    sentence = request.args.get('sentence')
+    sentence = request.args.get("sentence")
     positive = sentence_prediction(sentence)
     negative = 1 - positive
     response = {}
-    response["response"] = {
+    response['response'] = {
         "positive": str(positive),
         "negative": str(negative),
         "sentence": str(sentence),
     }
+    print(flask.jsonify(response))
     return flask.jsonify(response)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     model = BertSentimentClassifier()
-    model.load_state_dict(torch.load(config.model_path))
-    model.to(config.device)
+    model.load_state_dict(torch.load(config.MODEL_PATH))
+    model.to(config.DEVICE)
     model.eval()
-    app.run(host='0.0.0.0', port='9999')
-
-
-
-
-
-
-
+    app.run()
