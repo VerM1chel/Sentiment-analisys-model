008baa998c7d6ff7009da5357a1783cbc128ef81
VerM1chel <86617861+VerM1chel@users.noreply.github.com>
2022-02-20 23:52:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import transformers
 
-train_batch = 10
-valid_batch = 5
-epochs = 10
-max_length = 512
+TRAIN_BATCH = 10
+VALID_BATCH = 5
+EPOCHS = 10
+MAX_LENGTH = 512
 
-bert_path = './bert-base-cased'
-train_data = './imdb.csv'
-model_path = './checkpoint.pth'
-device = 'cuda'
-tokenizer = transformers.BertTokenizer.from_pretrained(bert_path, do_lower_case=True)
+BERT_PATH = './bert-base-cased'
+TRAIN_DATA = './imdb.csv'
+MODEL_PATH = './checkpoint.pth'
+DEVICE = 'cuda'
+TOKENIZER = transformers.BertTokenizer.from_pretrained(BERT_PATH)
