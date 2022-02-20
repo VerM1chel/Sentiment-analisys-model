cd6828faf07a2fc6db2183622e70f4287ebe3b59
VerM1chel <86617861+VerM1chel@users.noreply.github.com>
2022-02-20 23:59:53 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,3 @@
-import config
-import dataset
 from model import BertSentimentClassifier
 import engine
 import numpy as np
@@ -11,6 +9,8 @@ from transformers import AdamW
 from transformers import get_linear_schedule_with_warmup
 import re
 
+import config
+import dataset
 
 def clean_text(texts):
     for i, text in enumerate(texts):
@@ -19,9 +19,8 @@ def clean_text(texts):
         texts[i] = re.sub("'ve", ' have', text)
     return texts
 
-
 def run():
-    data = pd.read_csv(config.train_data).fillna('None')
+    data = pd.read_csv(config.TRAIN_DATA).fillna('None')
     data['sentiment'] = data['sentiment'].apply(lambda x: 1 if x == 'positive' else 0)
     data['review'] = clean_text(data['review'])
 
@@ -30,10 +29,10 @@ def run():
     valid = valid.reset_index(drop=True)
 
     trainDataset = dataset.BertDataset(train['review'].values, train['sentiment'].values)
-    trainDataloader = torch.utils.data.DataLoader(trainDataset, batch_size=config.train_batch, num_workers=4)
+    trainDataloader = torch.utils.data.DataLoader(trainDataset, batch_size=config.TRAIN_BATCH, num_workers=4)
 
     validDataset = dataset.BertDataset(valid['review'].values, valid['sentiment'].values)
-    validDataloader = torch.utils.data.DataLoader(validDataset, batch_size=config.valid_batch, num_workers=1)
+    validDataloader = torch.utils.data.DataLoader(validDataset, batch_size=config.VALID_BATCH, num_workers=1)
 
     device = torch.device('cuda')
     model = BertSentimentClassifier()
@@ -45,30 +44,27 @@ def run():
         {'params': [param for n, param in parameters if not any(nd in n for nd in no_decay)],
          'weight_decay': 0.001},
         {'params': [param for n, param in parameters if any(nd in n for nd in no_decay)],
-         'weight_decay': 0.0}  # !
+         'weight_decay': 0.0}
     ]
 
     optimizer = AdamW(optimizer_parameters, lr=5e-5)
 
-    num_training_steps = int(len(trainDataset) / config.train_batch * config.epochs)
-    scheduler = get_linear_schedule_with_warmup(optimizer=optimizer, num_warmup_steps=0,
+    num_training_steps = int(len(trainDataset) / config.TRAIN_BATCH * config.EPOCHS)
+    scheduler = get_linear_schedule_with_warmup(optimizer=optimizer,
+                                                num_warmup_steps=0,
                                                 num_training_steps=num_training_steps)
 
     best_accuracy = 0
-    for epoch in range(config.epochs):
-        print(f'epoch_{epoch + 1}')
+    for epoch in range(config.EPOCHS):
+        print(f'epoch_{epoch+1}')
         engine.train_fn(trainDataloader, model, optimizer, device, scheduler)
         outputs, targets = engine.eval_fn(validDataloader, model, device)
         outputs = np.array(outputs) > 0.5
         accuracy = metrics.accuracy_score(outputs, targets)
         print(f'Accuracy score = {accuracy}')
         if accuracy > best_accuracy:
-            print('nice')
             best_accuracy = accuracy
-            torch.save(model.state_dict(), config.model_path)
-            torch.save(optimizer.state_dict(), '/kaggle/working/opt.pth')  # !
-            torch.save(scheduler.state_dict(), '/kaggle/working/shed.pth')  # !
-
+            torch.save(model.state_dict(), config.MODEL_PATH)
 
 if __name__ == '__main__':
     run()
